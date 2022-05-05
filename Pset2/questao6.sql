24ef240e8b0bc373dc8d7edeb52ef70e1d29183b
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2022-05-05 19:14:25 -0300

    Update

--- Pset2/questao6.sql
+++ Pset2/questao6.sql
@@ -1,17 +1,19 @@
 SELECT CONCAT(primeiro_nome," ",nome_meio," ",ultimo_nome) "Nome completo",
 CONCAT(d.nome_departamento,",n°", d.numero_departamento) "Departamento",
-(CASE
-WHEN f.sexo = 'M' THEN "Masculino"
-ELSE "Feminino"
-END) "sexo do funcionario",
+CASE
+WHEN f.sexo = 'M' THEN 'Masculino'
+ELSE 'Feminino'
+END "sexo do funcionario",
 nome_dependente,
 TIMESTAMPDIFF(year, dp.data_nascimento,CURDATE()) "idade",
-(CASE
-WHEN dp.sexo  = 'M' THEN "Masculino"
-ELSE "Feminino"
-END) "sexo do dependente"
+CASE
+WHEN dp.sexo  = 'M' THEN 'Masculino'
+ELSE 'Feminino'
+END "sexo do dependente"
 FROM funcionario f
 INNER JOIN dependente dp
 ON f.cpf = dp.cpf_funcionario
 INNER JOIN departamento d
-ON f.numero_departamento = d.numero_departamento;
+ON f.numero_departamento = d.numero_departamento
+ORDER BY f.numero_departamento, 
+primeiro_nome;
