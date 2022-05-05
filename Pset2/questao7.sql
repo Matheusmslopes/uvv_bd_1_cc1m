45154f2bbef360fd7d334d534684fba39b16ce50
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2022-05-05 19:09:36 -0300

    Update

--- Pset2/questao7.sql
+++ Pset2/questao7.sql
@@ -1,1 +1,10 @@
-SELECT
+SELECT CONCAT(primeiro_nome," ",nome_meio," ",ultimo_nome) "Nome completo",
+CONCAT(d.nome_departamento,",n°", d.numero_departamento) "Departamento",
+salario
+FROM funcionario f
+INNER JOIN departamento d
+ON f.numero_departamento = d.numero_departamento
+LEFT OUTER JOIN dependente dp
+ON f.cpf = dp.cpf_funcionario 
+WHERE dp.cpf_funcionario IS NULL
+ORDER BY f.numero_departamento;
