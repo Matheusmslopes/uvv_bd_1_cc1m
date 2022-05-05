c732db7a9e970dba58855afa269a9db082f6468e
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2022-05-05 19:16:05 -0300

    Update

--- Pset2/Questao4.sql
+++ Pset2/Questao4.sql
@@ -1,8 +1,8 @@
 SELECT CONCAT(primeiro_nome," ",nome_meio," ",ultimo_nome) "Nome completo",
 TIMESTAMPDIFF(year, data_nascimento,CURDATE()) "idade",
 salario "Salário atual",
-(CASE
+CASE
 WHEN salario <35000 THEN salario*1.2
 ELSE salario*1.15
-END) "Novo salário"
+END "Novo salário"
 FROM funcionario;
