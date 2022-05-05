972062e13cf9bc27308cb637502c855a5c64b8e3
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2022-05-05 17:11:16 -0300

    Create

---
+++ Pset2/questao5.sql
@@ -0,0 +1,14 @@
+SELECT CONCAT(d.nome_departamento,",n°", d.numero_departamento) "Departamento",
+(CASE
+WHEN f.cpf = d.cpf_gerente THEN CONCAT(f.primeiro_nome," ", f.nome_meio," ", f.ultimo_nome)
+ELSE "-"
+END) "Gerência",
+(CASE
+WHEN f.cpf != d.cpf_gerente THEN CONCAT(f.primeiro_nome," ", f.nome_meio," ", f.ultimo_nome)
+ELSE "-"
+END) "Funcionário"
+FROM funcionario f
+INNER JOIN departamento d
+ON f.numero_departamento = d.numero_departamento
+ORDER BY d.nome_departamento ASC,
+f.salario DESC;
