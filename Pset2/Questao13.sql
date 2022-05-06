352ab7781ae42fa3b5b611c7edfdc92d703f5354
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2022-05-06 18:18:33 -0300

    Update

--- Pset2/Questao13.sql
+++ Pset2/Questao13.sql
@@ -1,1 +1,16 @@
-SELECT CREATE
+SELECT CONCAT(primeiro_nome," ",nome_meio," ",ultimo_nome) "Nome completo",
+TIMESTAMPDIFF(year, f.data_nascimento,CURDATE()) "idade",
+CASE
+WHEN f.sexo = 'M' THEN 'Masculino'
+WHEN f.sexo = 'F' THEN 'Feminino'
+END "sexo"
+FROM funcionario f 
+UNION 
+SELECT dp.nome_dependente,
+TIMESTAMPDIFF(year, dp.data_nascimento,CURDATE()) "idade",
+CASE
+WHEN dp.sexo = 'M' THEN 'Masculino'
+WHEN dp.sexo = 'F' THEN 'Feminino'
+END "sexo"
+FROM dependente dp
+ORDER BY idade;
