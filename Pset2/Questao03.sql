ecfbbfde2f480e348a953be5b57d79ca722df1a1
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2022-05-06 18:54:45 -0300

    Update

--- Pset2/Questao03.sql
+++ Pset2/Questao03.sql
@@ -1,10 +1,10 @@
 SELECT CONCAT(primeiro_nome," ",nome_meio," ",ultimo_nome) "Nome completo", 
-data_nascimento, 
-timestampdiff(year, data_nascimento,CURDATE()) "idade", 
-salario, 
-d.nome_departamento, 
-d.numero_departamento
+data_nascimento "Data de nascimento", 
+timestampdiff(year, data_nascimento,CURDATE()) "Idade", 
+salario "Salário", 
+d.nome_departamento "Nome do departamento", 
+d.numero_departamento "Número do departamento"
 FROM funcionario f
 INNER JOIN departamento d
 ON f.numero_departamento = d.numero_departamento
-ORDER BY numero_departamento;
+ORDER BY d.numero_departamento;
