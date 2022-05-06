9ff5a320fb6882cf313d5f50068fa09721f57ff7
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2022-05-06 19:07:44 -0300

    Update

--- Pset2/Questao14.sql
+++ Pset2/Questao14.sql
@@ -1,4 +1,5 @@
-SELECT d.numero_departamento "número do departamento", COUNT(f.numero_departamento) "número de funcionários"
+SELECT d.numero_departamento "Número do departamento", 
+COUNT(f.numero_departamento) "Número de funcionários"
 FROM funcionario f
 INNER JOIN departamento d
 ON f.numero_departamento  = d.numero_departamento
