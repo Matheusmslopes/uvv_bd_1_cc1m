5b45eed8e0fc16e3296453a04a0603f239030e3c
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2022-05-06 16:46:43 -0300

    Update

--- Pset2/Questao12.sql
+++ Pset2/Questao12.sql
@@ -1,1 +1,11 @@
-SELECT 
+SELECT CONCAT(primeiro_nome," ",nome_meio," ",ultimo_nome) "Nome completo",
+p.nome_projeto "Projeto",
+d.nome_departamento "Departamento"
+FROM funcionario f
+INNER JOIN departamento d 
+ON f.numero_departamento = d.numero_departamento 
+INNER JOIN projeto p
+ON p.numero_departamento = d.numero_departamento 
+INNER JOIN trabalha_em t
+ON t.cpf_funcionario = f.cpf 
+WHERE t.horas IS NULL;
