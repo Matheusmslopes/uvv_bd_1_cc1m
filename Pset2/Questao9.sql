365fde822b9c1992ceb1fd08530712d8f5f59f70
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2022-05-06 16:12:07 -0300

    Update

--- Pset2/Questao9.sql
+++ Pset2/Questao9.sql
@@ -1,1 +1,10 @@
-SELECT
+SELECT CONCAT(d.nome_departamento,",n°", d.numero_departamento) "Departamento",
+p.nome_projeto,
+SUM(t.horas) "Horas trabalhadas"
+FROM trabalha_em t
+INNER JOIN projeto p 
+ON t.numero_projeto = p.numero_projeto 
+INNER JOIN departamento d 
+ON d.numero_departamento = p.numero_departamento
+GROUP BY p.nome_projeto
+ORDER BY d.numero_departamento;
