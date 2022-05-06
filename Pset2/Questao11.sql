2468732d297fd214932ae18f8878e9b8ca5bc5e1
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2022-05-06 16:36:14 -0300

    Update

--- Pset2/Questao11.sql
+++ Pset2/Questao11.sql
@@ -1,1 +1,9 @@
-SELECT
+SELECT CONCAT(primeiro_nome," ",nome_meio," ",ultimo_nome) "Nome completo",
+p.nome_projeto,
+50*t.horas "valor por hora trabalhada"
+FROM funcionario f
+INNER JOIN trabalha_em t
+ON f.cpf = t.cpf_funcionario 
+INNER JOIN projeto p 
+ON t.numero_projeto = p.numero_projeto
+ORDER BY "Nome completo";
