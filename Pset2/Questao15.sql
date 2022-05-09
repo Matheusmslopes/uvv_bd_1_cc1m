467abb20c99828e4641d11a4b36f00a0461d2267
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2022-05-09 18:50:21 -0300

    Update

--- Pset2/Questao15.sql
+++ Pset2/Questao15.sql
@@ -1,1 +1,11 @@
-
+SELECT CONCAT(primeiro_nome," ",nome_meio," ",ultimo_nome) "Nome completo",
+d.nome_departamento "Nome do departamento",
+p.nome_projeto "Nome do projeto"
+FROM trabalha_em t
+LEFT OUTER JOIN funcionario f 
+ON t.cpf_funcionario = f.cpf
+INNER JOIN departamento d 
+ON f.numero_departamento = d.numero_departamento
+LEFT OUTER JOIN projeto p 
+ON t.numero_projeto = p.numero_projeto
+ORDER BY f.numero_departamento;
