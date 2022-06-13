5cff1a92dbcd4fbef44a214eee91a93ff37cd380
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2022-06-13 18:59:18 -0300

    Create

---
+++ Pset3/QueryRecursiva.sql
@@ -0,0 +1,24 @@
+WITH RECURSIVE tabela_pai AS (
+SELECT 
+codigo,
+nome,
+codigo_pai,
+1 AS nivel,
+CAST(nome AS TEXT) AS niv_relacao
+FROM classificacao
+WHERE codigo_pai IS NULL 
+UNION ALL
+SELECT c.codigo,
+tp.nome,
+c.codigo_pai,
+tp.nivel + 1 AS nivel,
+CAST(tp.niv_relacao || ' -->> ' || c.nome AS TEXT) AS niv_relacao
+FROM classificacao c
+INNER JOIN tabela_pai tp 
+ON c.codigo_pai = tp.codigo)
+SELECT 
+nivel AS "Nível",
+niv_relacao AS "Relação",
+codigo_pai AS "Código do produto Pai"
+FROM tabela_pai tp
+ORDER BY niv_relacao;
